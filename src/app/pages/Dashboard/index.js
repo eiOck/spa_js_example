3a6a651c47e467d88118baa03dba4439d68f37cd
Michal Macinka <michal.macinka@uhk.cz>
2018-11-26 14:01:29 +0100

    updated

--- src/app/pages/Dashboard/index.js
+++ src/app/pages/Dashboard/index.js
@@ -3,17 +3,6 @@ import logo from '../../assets/images/logo.svg';
 
 class Dashboard extends React.Component {
 
-    componentDidMount = () => {
-        /*fetch('https://stag-demo.uhk.cz/ws/services/rest/programy/getStudijniProgramy?fakulta=FIM&outputFormat=JSON',
-        {
-            mode: 'no-cors',
-        })
-        .then(res => res.text().then(data => data.parse()))
-        .then(text => console.log(text))
-        // .then(data => console.log(data))
-	*/
-    }
-
     render(){
         return (
             <div className="App">
